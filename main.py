faaa34607ec26b9ff4c4db42b68611442b87727d
Anushree Sabade <68522247+anushree35@users.noreply.github.com>
2026-02-19 20:06:37 -0500

    Implement main function to load university data

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from university import University
+from loader import load_course_catalog, load_students
 
+def main():
+    uni = University()
+    load_course_catalog("course_catalog.csv", uni)
+    load_students("university_data.csv", uni)
+
+    # test a quick query
+    print(uni.get_course_enrollment("CSE2050"))
+
+if __name__ == "__main__":
+    main()
