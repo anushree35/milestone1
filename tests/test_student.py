f5ad27f14f63dc76465c37fa23a2f9c8be25d8ee
Anushree Sabade <68522247+anushree35@users.noreply.github.com>
2026-03-04 21:23:12 -0500

    Update student ID format and course names in tests

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,32 +1,26 @@
+import sys
+sys.path.insert(0, '..')
 import unittest
-
-class Course:
-    def __init__(self, code, credits):
-        self.code = code
-        self.credits = credits
-        self.roster = []
-
-    def enroll(self, student):
-        self.roster.append(student)
+from student import Student
+from course import Course
 
 class TestStudent(unittest.TestCase):
     def setUp(self):
-        self.student = Student("008", "Prudhvi")
+        self.student = Student("STU00008", "Prudhvi")
         self.course1 = Course("CSE2050", 3)
-        self.course2 = Course("MATH2110Q", 4)
-
+        self.course2 = Course("MATH2010", 4)
+    
     def test_student_creation(self):
-        self.assertEqual(self.student.student_id, "008")
+        self.assertEqual(self.student.student_id, "STU00008")
         self.assertEqual(self.student.name, "Prudhvi")
         self.assertEqual(self.student.courses, {})
 
     def test_enroll(self):
         self.student.enroll(self.course1, 'F')
-
         self.assertIn(self.course1, self.student.courses)
         self.assertEqual(self.student.courses[self.course1], 'F')
         self.assertIn(self.student, self.course1.students)
-        
+
     def test_calculate_gpa(self):
         self.student.enroll(self.course1, 'B')
         self.student.enroll(self.course2, 'A')
@@ -40,16 +34,11 @@ class TestStudent(unittest.TestCase):
 
     def test_get_course_info(self):
         self.student.enroll(self.course1, 'C')
-        information = self.student.get_course_info()
-        self.assertEqual(len(information), 1)
-        self.assertEqual(information[0]['course_code'], "CSE2050")
-        self.assertEqual(information[0]['credits'], 3)
-        self.assertEqual(information[0]['grade'], 'C')
+        info = self.student.get_course_info()
+        self.assertEqual(len(info), 1)
+        self.assertEqual(info[0]['course_code'], "CSE2050")
+        self.assertEqual(info[0]['credits'], 3)
+        self.assertEqual(info[0]['grade'], 'C')
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-   
-
