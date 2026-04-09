162a5c59cc46ce36c2a3bbada6ae9ab49b953444
prudhvip007 <prudhvi.p2025@gmail.com>
2026-04-08 20:30:09 -0400

    Rename enrollment_date to enroll_date in EnrollmentRecord

--- milestone2/enrollment_record.py
+++ milestone2/enrollment_record.py
@@ -2,13 +2,13 @@ import datetime
 
 class EnrollmentRecord:
     
-    def __init__(self, student, enrollment_date):
+    def __init__(self, student, enroll_date):
 
-        if isinstance(enrollment_date, datetime.date):
-            enroll_date = datetime.fromisoformat(enrollment_date)
+        if isinstance(enroll_date, datetime.date.fromisoformat(enroll_date)):
+            enroll_date = datetime.fromisoformat(enroll_date)
 
         self.student = student 
-        self.enrollment_date = enroll_date
+        self.enroll_date = enroll_date
 
     def __repr__(self):
-        return f"EnrollmentRecord(student={self.student}, enrollment_date={self.enrollment_date})"
+        return f"EnrollmentRecord(student={self.student}, enroll_date={self.enroll_date})"
