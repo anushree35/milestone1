31d3b64984c65e8c73fca9d9b9170339ff830046
prudhvip007 <prudhvi.p2025@gmail.com>
2026-04-29 17:34:16 -0400

    Refactor merge_sort and quick_sort functions

--- milestone2/milestone3/sorting.py
+++ milestone2/milestone3/sorting.py
@@ -1,22 +1,23 @@
+from heapq import merge
+
+
 def merge_sort(list, key=None, reverse=False):
     if len(list) <= 1:
         return list
 
     mid = len(list) // 2
-    left_half = merge_sort(list[:mid], key=key, reverse=reverse)
-    right_half = merge_sort(list[mid:], key=key, reverse=reverse)
+    left = merge_sort(list[:mid], key=key, reverse=reverse)
+    right = merge_sort(list[mid:], key=key, reverse=reverse)
 
-    return merge(left_half, right_half, key=key, reverse=reverse)
+    return merge(left, right, key=key, reverse=reverse)
 
 def _merge(left, right, key=None, reverse=False):
     merged = []
     i = j = 0
 
     while i < len(left) and j < len(right):
-        left_key = key(left[i]) if key else left[i]
-        right_key = key(right[j]) if key else right[j]
 
-        if (left_key > right_key) if reverse else (left_key <= right_key):
+        if (key(left[i]) > key(right[j])) if reverse else (key(left[i]) <= key(right[j])):
             merged.append(left[i])
             i += 1
         else:
@@ -25,18 +26,17 @@ def _merge(left, right, key=None, reverse=False):
 
     merged.extend(left[i:])
     merged.extend(right[j:])
-
     return merged
 
 def quick_sort(list, key=None, reverse=False):
     if len(list) <= 1:
         return list
+    k = key or (lambda x: x)
+    pivot = k(list[len(list) // 2])
 
-    pivot = key(lst[len(lst) // 2]) if key else lst[len(lst) // 2]
- 
-    less    = [x for x in lst if (key(x) if key else x) < pivot]
-    equal   = [x for x in lst if (key(x) if key else x) == pivot]
-    greater = [x for x in lst if (key(x) if key else x) > pivot]
+    less    = [x for x in list if k(x) < pivot]
+    equal   = [x for x in list if k(x) == pivot]
+    greater = [x for x in list if k(x) > pivot]
  
     if reverse:
         return quick_sort(greater, key=key, reverse=True) + equal + quick_sort(less, key=key, reverse=True)
